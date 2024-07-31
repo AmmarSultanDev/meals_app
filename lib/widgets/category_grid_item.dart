ebbd166697cfb59b9e5a089b7a5908f2a7873387
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-07-31 09:53:08 +0100

    chore: Refactor `categories.dart` and `category_grid_item.dart

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -9,6 +9,7 @@ class CategoryGridItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [
@@ -18,6 +19,13 @@ class CategoryGridItem extends StatelessWidget {
           begin: Alignment.topLeft,
           end: Alignment.bottomRight,
         ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge!
+            .copyWith(color: Theme.of(context).colorScheme.onSurface),
       ),
     );
   }
