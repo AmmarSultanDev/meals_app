7f64dd6f469273cf9c1fa827e6895849f9594300
AmmarSultanDev <ammarsultan1445@gmail.com>
2024-07-31 10:42:42 +0100

    chore: Update CategoriesScreen to use MealsScreen for category selection

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(title: 'Some title', meals: [])));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +28,9 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+                category: category,
+                onSelectCategory: () => _selectCategory(context)),
         ],
       ),
     );
